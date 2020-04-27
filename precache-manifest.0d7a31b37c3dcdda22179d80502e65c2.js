e775f636b34d4a05f51336f7b5e7db5d6ae65acb
ritik singh <mr.ritik99@gmail.com>
2020-04-27 21:46:45 +0530

    Updates

---
+++ precache-manifest.0d7a31b37c3dcdda22179d80502e65c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78470dc3c7e1b286566706b923d428bc",
+    "url": "/Blog_Escape/index.html"
+  },
+  {
+    "revision": "a4cce0be219e6f167df7",
+    "url": "/Blog_Escape/static/css/main.6ab265cf.chunk.css"
+  },
+  {
+    "revision": "442da12b9b92901ef31b",
+    "url": "/Blog_Escape/static/js/2.fcf57fce.chunk.js"
+  },
+  {
+    "revision": "edae9195774d0a68d3d5884ba3b463c4",
+    "url": "/Blog_Escape/static/js/2.fcf57fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4cce0be219e6f167df7",
+    "url": "/Blog_Escape/static/js/main.16f19638.chunk.js"
+  },
+  {
+    "revision": "6be5977e5ba38275c018",
+    "url": "/Blog_Escape/static/js/runtime-main.32ab9879.js"
+  }
+]);
